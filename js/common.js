ea5f754feede2de1ea064b3785c416bda3405287
BHLim08 <illust_gyubin@naver.com>
2025-04-17 17:57:12 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,19 +1,4 @@
 
-    $(function(){
-        function topview() {
-        $('.topview').stop().animate({
-            marginLeft: '-100vw'
-        }, 1500, function () {
-            $('.topview div:first').appendTo('.topview');
-            $('.topview').css({ marginLeft: 0 });
-        });
-    }
-    setInterval(topview, 3500);})
-
-
-
-    // //헤더컬러체인지
-
         $(function () {
             $('#menu2').click(function () {
                 $('#menu').slideToggle();
